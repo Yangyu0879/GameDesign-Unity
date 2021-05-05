778be9f0ecadf530132b64e57fcd5e8879162e4c
NeKoSaNnn <nekomjx@qq.com>
2021-05-05 20:24:07 +0800

    人物绳子存在bug，部分特效加入

--- twod-platformer-unity/Assets/Scripts/Enemy/Enemy.cs
+++ twod-platformer-unity/Assets/Scripts/Enemy/Enemy.cs
@@ -8,6 +8,7 @@ public abstract class Enemy : MonoBehaviour
     public int damage;
     //flash when damaged
     public float flashTime = 0.2f;
+    public float knockbackFromWep = 2500f;
 
     private SpriteRenderer rendr;
     private Color originalColor;
--- twod-platformer-unity/Assets/Scripts/RopeHook/throwhook.cs
+++ twod-platformer-unity/Assets/Scripts/RopeHook/throwhook.cs
@@ -6,13 +6,18 @@ public class throwhook : MonoBehaviour {
 
 	public GameObject hook;
 
-
+    //绳索发出与否
 	public bool ropeActive;
 
-	GameObject curHook;
+	public GameObject curHook;
+
+    public float ropeLength = 6f;
 
     public LayerMask grappleMask;
-    public float boostForce = 10000f;
+
+    public float launchForce = 1000f;
+
+    public float offset=-90;
 
     // Use this for initialization
     void Start () {
@@ -21,78 +26,64 @@ public class throwhook : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        launchForce = 1000f;
 
-
+        //发绳索
 		if (Input.GetMouseButtonDown (0)) {
-
+            //鼠标点击点为目的地
+            Vector2 dest = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
 			if (ropeActive == false) {
-                //Vector2 destiny = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-
-
-                //curHook = (GameObject)Instantiate (hook, transform.position, Quaternion.identity);
-
-                //curHook.GetComponent<RopeScript> ().destiny = destiny;
-                // RaycastHit2D hit = Physics2D.Raycast(destiny, Vector2.zero);
-                Vector2 destiny = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                //Debug.Log(destiny - (Vector2)transform.position);
-                RaycastHit2D hit = Physics2D.Raycast(transform.position, destiny - (Vector2)transform.position, 30, grappleMask); // shoot raycast from player to destiny of length ropeLength
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, dest - (Vector2)transform.position, ropeLength, grappleMask); // shoot raycast from player to destiny of length ropeLength
                 //Debug.Log(hit.collider != null ? hit.collider.transform.tag : "nothing");
-                if (hit.collider != null) //&& hit.collider.transform.tag != "Ungrappable")
+                if (hit.collider != null&& hit.collider.transform.tag != "Ungrappable")//某些场景Tag设置为不可抓取
                 {
                     //AudioManager.instance.Play("HookHit");
                     //curHook = (GameObject)Instantiate(hook, transform.position, Quaternion.identity);
                     Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
                     float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-                    curHook = (GameObject)Instantiate(hook, transform.position, Quaternion.Euler(0f, 0f, rotZ -90));
-                    curHook.GetComponent<RopeScript>().destiny = hit.point;
+                    curHook = (GameObject)Instantiate(hook, transform.position, Quaternion.Euler(0f, 0f, rotZ+offset));
+                    curHook.GetComponent<RopeScript>().dest = hit.point;
 
                     ropeActive = true;
 
                 }
-
-                //ropeActive = true;
-			} else {
-
-				//delete rope
-
-				Destroy (curHook);
-
-
-				ropeActive = false;
-
-			}
+			} 
 		}
+
+        //收绳索
         if (Input.GetMouseButtonDown(1))
         {
             if (ropeActive) // && cc.m_Grounded == false)
             {
+                //if(!canJump) launchForce += 250f; // slightly higher boostforce when player is grounded
 
-                //boostForce += 250f; // slightly higher boostforce when player is grounded
-
-                Debug.Log(boostForce);
                 // gameObject.GetComponent<Rigidbody2D>().AddForce(gameObject.GetComponent<Rigidbody2D>().velocity * 400); // use this for super boost
-                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 400f)); // boost player upwards when rope is withdrawn
-                //gameObject.GetComponentInChildren<TrailRenderer>().enabled = false; // disable green trial
-                //gameObject.transform.Find("Boost Trail").gameObject.SetActive(true);  // enable boost trial
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, launchForce)); // boost player upwards when rope is withdrawn
+                gameObject.transform.Find("Trail").gameObject.SetActive(false);  // disable green trial
+                gameObject.transform.Find("launchTrail").gameObject.SetActive(true);  // enable boost trial
 
-                //Invoke("revertTrail", 0.7f); // change trail renderer back to original
-                Destroy(curHook);
-                ropeActive = false;
+                Invoke("recoveryTrail", 0.7f); // change trail renderer back to original
             }
-
-            //delete rope
-
-            
-
-
-
+            resetRope();
         }
-        
 
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(ropeActive)
+            {
+                resetRope();
+            }
+        }
 
     }
+
+    void recoveryTrail()
+    {
+        gameObject.transform.Find("Trail").gameObject.SetActive(true);  // disable green trial
+        gameObject.transform.Find("launchTrail").gameObject.SetActive(false);  // enable boost trial
+    }
+
     public void resetRope()
     {
         Destroy(curHook);
