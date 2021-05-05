532e7d3a16d105a30179628a43d2e8fff7b44c31
NeKoSaNnn <nekomjx@qq.com>
2021-05-05 22:16:27 +0800

    虚拟相机加入+Tilemap加入

---
+++ twod-platformer-unity/Assets/Scripts/Crystals.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Crystals : MonoBehaviour
+{
+
+    private PlayerMovement player;
+    private throwhook throwHook;
+    private bool collected = false;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>();
+        throwHook = FindObjectOfType<throwhook>(); // find throwhook script
+    }
+
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (!collected)
+            {
+                collected = true;
+                AudioManager.instance.Play("Collect");
+                Destroy(gameObject);
+                player.crystals++;
+            }
+        }
+         else if (other.tag == "Grappling Hook")
+        {
+            if (!collected)
+            {
+                collected = true;
+                AudioManager.instance.Play("Collect");
+                Destroy(gameObject);
+                player.crystals++;
+                //throwHook.resetRope();
+            }
+        }
+    }
+}
---
+++ twod-platformer-unity/Assets/Scripts/GoldCoin.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldCoin : MonoBehaviour
+{
+    private PlayerMovement player;
+    private throwhook throwHook;
+    private bool collected = false;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>();
+        throwHook = FindObjectOfType<throwhook>(); // find throwhook script
+    }
+
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (!collected)
+            {
+                collected = true;
+                AudioManager.instance.Play("Coin");
+                Destroy(gameObject);
+                player.goldCoins++;
+            }
+        }
+        else if (other.tag == "Grappling Hook")
+        {
+            if (!collected)
+            {
+                collected = true;
+                AudioManager.instance.Play("Coin");
+                Destroy(gameObject);
+                player.goldCoins++;
+                //Destroy(throwHook.curHook);
+                //throwHook.ropeActive = false;
+            }
+        }
+    }
+}
